5387f9b927d66964732a0e19d30983518b396064
ationge4568temat <admin@mdavid.cn>
2026-08-11 12:19:12 +0800

    chore: add github action for IPA build and update bundle ids to ssadtyer.top

--- PacketTunnel/Sources/MWAppGroup.m
+++ PacketTunnel/Sources/MWAppGroup.m
@@ -1,6 +1,6 @@
 #import "MWAppGroup.h"
 
-NSString *const MWAppGroupIdentifier = @"group.com.tangzixiang.meow";
+NSString *const MWAppGroupIdentifier = @"group.ssadtyer.top";
 
 @implementation MWAppGroup
 
