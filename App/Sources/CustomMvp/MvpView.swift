1ac1dec5abddebbb494627addee3063a9a75ff3d
ationge4568temat <admin@mdavid.cn>
2026-08-18 00:28:14 +0800

    upt

--- App/Sources/CustomMvp/MvpView.swift
+++ App/Sources/CustomMvp/MvpView.swift
@@ -337,7 +337,7 @@ struct MvpProfileCard: View {
                     .foregroundColor(MvpTheme.textPrimary)
 
                 Text("配置文件")
-                    .font(.system(size: 16, weight: .bold))
+                    .font(.system(size: 16, weight: .semibold))
                     .foregroundColor(MvpTheme.textPrimary)
             }
 
