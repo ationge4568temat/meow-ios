d2b96eb52a10ead1cd823aaa6023fad7b24f95c5
ationge4568temat <admin@mdavid.cn>
2026-08-28 17:03:44 +0800

    upt

--- App/Sources/CustomMvp/MvpDevice.swift
+++ App/Sources/CustomMvp/MvpDevice.swift
@@ -13,6 +13,7 @@ public enum MvpDevice {
     /// - 无需配置 Keychain 或特殊 Entitlements
     /// - 同一个开发商的 App 共享相同的标识符
     /// - 仅在卸载设备上所有该开发商的 App 时才会重置，足够满足长效标识需求。
+    @MainActor
     public static var hwid: String {
         if let id = UIDevice.current.identifierForVendor?.uuidString {
             log.debug("Obtained HWID from identifierForVendor")
